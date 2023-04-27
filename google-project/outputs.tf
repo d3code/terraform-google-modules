f1b075adb8a5a86029cb4bec5dd5c25142bcc14b
Luke Sands <luke.sands@d3code.com.au>
2023-04-28 02:08:18 +1000

    Updates

--- google-project/outputs.tf
+++ google-project/outputs.tf
@@ -1,3 +1,23 @@
 output "project_id" {
   value = google_project.project.project_id
 }
+
+output "number" {
+  value = google_project.project.number
+}
+
+output "org_id" {
+  value = google_project.project.org_id
+}
+
+output "name" {
+  value = google_project.project.name
+}
+
+output "region" {
+  value = google_project.project.region
+}
+
+output "zone" {
+  value = google_project.project.zone
+}
